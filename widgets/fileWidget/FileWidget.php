a76034f369296304a67271addf9daddde1b1c2b5
osadrum <osadrum@gmail.com>
2016-02-12 14:20:37 +0300

    update

--- widgets/fileWidget/FileWidget.php
+++ widgets/fileWidget/FileWidget.php
@@ -21,8 +21,6 @@ class FileWidget extends Widget
 
     public $jsCallbackFunctionDone = '';
 
-    public $jsCallbackFunctionDone = '';
-
     public function run()
     {
         if ($this->model != null && $this->attribute != null) {
